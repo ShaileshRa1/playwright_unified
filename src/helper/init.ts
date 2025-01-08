cc584ec059152f8ef8bdfd6160c390542ba7500b
shaileshrahinj <rahinj.shailesh66@gmail.com>
2025-01-08 15:21:40 +0000

    report changes, parallel execution, rerun
    added fs extra(file interaction)
    parallel execution
    rerun functionality
    updated gitIgnore

---
+++ src/helper/init.ts
@@ -0,0 +1,12 @@
+const fs = require ("fs-extra");
+
+try{
+    fs.ensureDir("test-result");
+    fs.emptyDir("test-result")
+}
+
+    catch(error){
+        console.log("Folder not created!" +error);
+
+    }
+    
--- src/helper/report.ts
+++ src/helper/report.ts
@@ -1,27 +1,24 @@
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "test-result",
-  reportPath: "./",
+  jsonDir: "./test-result/",
+  reportPath: "./test-result/",
   metadata: {
     browser: {
-      name: "chrome",
-      version: "60",
+      name: "Chromium",
+      version: "131.0.6778.33 ",
     },
     device: "Local test machine",
     platform: {
-      name: "chrome",
-      version: "16.04",
+      name: "windows",
+      version: "11",
     },
   },
   customData: {
     title: "Run info",
     data: [
-      { label: "Project", value: "Custom project" },
-      { label: "Release", value: "1.2.3" },
-      { label: "Cycle", value: "2.12" },
-    //   { label: "Execution Start Time", value: "Nov 19th 2017, 02:31 PM EST" },
-    //   { label: "Execution End Time", value: "Nov 19th 2017, 02:56 PM EST" },
+      { label: "Project", value: "Playwright test execution" },
+      { label: "Release", value: "1.0.1" },
     ],
   },
 });
